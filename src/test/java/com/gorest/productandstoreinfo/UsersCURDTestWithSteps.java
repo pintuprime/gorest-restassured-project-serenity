d0a2c12ef6e09c2ac3efcb6edf163014b12116e7
pintuprime <pintu.prime@25gmail.com>
2024-01-23 20:28:37 +0000

    Initial commit

---
+++ src/test/java/com/gorest/productandstoreinfo/UsersCURDTestWithSteps.java
@@ -0,0 +1,64 @@
+package com.gorest.productandstoreinfo;
+
+import com.gorest.testbase.TestBase;
+import com.gorest.userandpostinfo.UsersSteps;
+import com.gorest.utils.TestUtils;
+import io.restassured.response.ValidatableResponse;
+import net.serenitybdd.annotations.Steps;
+import net.serenitybdd.annotations.Title;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(SerenityRunner.class)
+public class UsersCURDTestWithSteps extends TestBase {
+
+    static String name = TestUtils.getRandomValue() + "Tom";
+    static String email = TestUtils.getRandomValue() + "tom@gmail.com";
+    static String gender = "male";
+    static String status = "active";
+
+    static int userId;
+
+    @Steps
+    UsersSteps steps;
+    @Title("This will create new user")
+    @Test
+    public void test001(){
+
+        ValidatableResponse response = steps.createUser(name, email,gender,status).statusCode(201);
+
+       userId = response.extract().jsonPath().getInt("id");
+        System.out.println(userId);
+    }
+
+    @Title("Verify the user added to the application")
+    @Test
+    public void test002(){
+
+        String userList = steps.getUserInfoByName(userId);
+
+        Assert.assertEquals(name, userList);
+    }
+
+    @Title("Update and verify user information")
+    @Test
+    public void test003(){
+
+        steps.partiallyUpdateUserInformation(userId, name, email, gender, status).statusCode(200);
+
+        String userList = steps.getUserInfoByName(userId);
+
+        Assert.assertEquals(name, userList);
+    }
+
+    @Title("Delete the user and verify if the user deleted")
+    @Test
+    public void test004(){
+
+        steps.deleteUser(userId).statusCode(204);
+        steps.getUserById(userId).statusCode(404);
+    }
+}
